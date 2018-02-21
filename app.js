5bed343d079456927fb357c5243c495277450418
vinayakolhapure <vinayakolhapure@aol.com>
2018-02-20 20:27:07 -0500

    using module exports

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-console.log('Starting app...');
+console.log('Starting app.js');
 
 const fs = require('fs');
 const os = require('os');
+const notes = require('./notes.js');
 
 var user = os.userInfo();
 
-fs.appendFile('greetings.txt', `Hello ${user.username}!`, function(err) {
+fs.appendFile('greetings.txt', `Hello ${user.username}! You are ${notes.age}.`, function(err) {
     if(err) {
         console.log('Unable to write to file');
     }
