eeea889512ceccacc3775d3eaa56933262f2902a
vinayakolhapure <vinayakolhapure@aol.com>
2018-02-21 00:49:35 -0500

    yargs - pass arguments and commands

--- notes.js
+++ notes.js
@@ -1,6 +1,24 @@
 console.log('Starting notes.js');
 
-module.exports.addNote = () => {
-    console.log('addNote');
-    return 'New Note';
+var addNote = (title,body) => {
+    console.log('Adding Note ', title, body);
+};
+
+var getAll = () => {
+    console.log('Listing notes');
+};
+
+var getNote = (title) => {
+    console.log('Getting Note: ', title);
+};
+
+var removeNote = (title) => {
+    console.log('Removing Note: ', title);
+};
+
+module.exports = {
+    addNote, //addNote: addNote
+    getAll,
+    getNote,
+    removeNote
 };
