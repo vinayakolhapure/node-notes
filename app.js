5e248813c79180293100332eab73a4b2e526808a
vinayakolhapure <vinayakolhapure@aol.com>
2018-02-21 19:48:28 -0500

    notes add complete

--- app.js
+++ app.js
@@ -14,7 +14,14 @@ console.log('Command: ', command);
 console.log('Yargs ', argv);
 
 if (command === 'add') {
-    notes.addNote(argv.title, argv.body);
+   var note = notes.addNote(argv.title, argv.body);
+   if (note) {
+    console.log(`Note created: Title: ${note.title}`);
+    console.log('--');
+    console.log(`Body: ${note.body}`);
+   } else {
+    console.log('Note title already taken');
+   }
 } else if (command === 'list') {
     notes.getAll(); 
 } else if (command === 'read') {
