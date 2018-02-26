6bf71e452ebf16da38fbb79410c8ef6b924318cf
vinayakolhapure <vinayakolhapure@aol.com>
2018-02-25 19:07:38 -0500

    configure yargs arguments

--- app.js
+++ app.js
@@ -4,12 +4,37 @@ const yargs = require('yargs');
 
 const notes = require('./notes.js');
 
-const argv = yargs.argv;
+const titleOptions = {
+    describe: 'Title of note',
+    demand: true,
+    alias: 't'
+};
+
+const bodyOptions = {
+    describe: 'Body of note',
+    demand: true,
+    alias: 'b'
+};
+
+const argv = yargs
+    .command('add','Adding a new note',{
+        title: titleOptions,
+        body: bodyOptions
+    })
+    .command('list','List all notes')
+    .command('read','Read a note',{
+        title: titleOptions
+    })
+    .command('remove', 'Removing a note',{
+        title: titleOptions
+    })
+    .help()
+    .argv;
 
 var command = argv._[0];
-console.log('Command: ', command);
+//console.log('Command: ', command);
 //console.log('Process ', process.argv);
-console.log('Yargs ', argv);
+//console.log('Yargs ', argv);
 
 if (command === 'add') {
    var note = notes.addNote(argv.title, argv.body);
